7896886ab8de8c3c814911230eb98045102b6669
mohaimen <mohaimen.jatri@gmail.com>
2022-07-04 00:57:12 +0600

    product update api create

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,18 +1,49 @@
 const Product = require('../models/ProductModel')
 
 //get all product
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({message: "Product successfully"})
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        status: true,
+        products
+    })
 }
 
 //create product
-
 exports.createProduct = async (req, res, next) => {
-    const product = await Product.create(req.body)
-    console.log(product)
-    res.status(201).json(
-        {
-            message: 'product created successfully'
-        }
-    )
+    try{
+        const product = await Product.create(req.body)
+        res.status(201).json(
+            {
+                status: true,
+                message: 'product created successfully',
+                product
+            }
+        )
+    } catch (e) {
+        res.status(400).json({
+            status: false,
+            errors: e.errors
+        })
+    }
+}
+
+//update product
+exports.updateProduct = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    if(!product) {
+        return res.status(400).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify:false
+    })
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
-const {getAllProducts, createProduct} = require('../controllers/productController')
+const {getAllProducts, createProduct, updateProduct} = require('../controllers/productController')
 
 router.route('/products').get(getAllProducts)
 router.route('/product/create').post(createProduct)
+router.route('/product/:id').put(updateProduct)
 module.exports = router;
